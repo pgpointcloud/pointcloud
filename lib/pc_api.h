8612f906a0d0e7376967dadf7f52575e757fe299
Paul Ramsey <pramsey@opengeo.org>
2013-10-23 12:03:27 -0700

    Set internal version to 1.0

--- lib/pc_api.h
+++ lib/pc_api.h
@@ -27,9 +27,9 @@
 
 
 #define POINTCLOUD_MAJOR_VERSION 1
-#define POINTCLOUD_MINOR_VERSION 1
+#define POINTCLOUD_MINOR_VERSION 0
 #define POINTCLOUD_PATCH_VERSION 0
-#define POINTCLOUD_VERSION "1.1.0"
+#define POINTCLOUD_VERSION "1.0.0"
 
 
 /**********************************************************************
