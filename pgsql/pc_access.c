1f733e6014089c4800474cbe4655eb52c7a4ced0
Vincent Picavet <vincent.picavet@oslandia.com>
2015-07-30 16:45:51 +0200

    Fix PG 9.5 compatibility.

--- pgsql/pc_access.c
+++ pgsql/pc_access.c
@@ -353,15 +353,7 @@ Datum pointcloud_agg_transfn(PG_FUNCTION_ARGS)
 		        (errcode(ERRCODE_INVALID_PARAMETER_VALUE),
 		         errmsg("could not determine input data type")));
 
-	if (fcinfo->context && IsA(fcinfo->context, AggState))
-	{
-		aggcontext = ((AggState *) fcinfo->context)->aggcontext;
-	}
-	else if (fcinfo->context && IsA(fcinfo->context, WindowAggState))
-	{
-		aggcontext = ((WindowAggState *) fcinfo->context)->aggcontext;
-	}
-	else
+	if ( ! AggCheckCallContext(fcinfo, &aggcontext) )
 	{
 		/* cannot be called directly because of dummy-type argument */
 		elog(ERROR, "pointcloud_agg_transfn called in non-aggregate context");
