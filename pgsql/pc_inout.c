88e2cc7380b34bbe50e9e3ea3389453d862c35e0
Mathieu Brédif <mathieu.bredif@ign.fr>
2016-03-08 12:16:29 +0100

    pc_envelope testing, degenerate cases, pc_envelope_asbinary

--- pgsql/pc_inout.c
+++ pgsql/pc_inout.c
@@ -31,7 +31,7 @@ Datum pcpoint_from_double_array(PG_FUNCTION_ARGS);
 Datum pcpoint_as_text(PG_FUNCTION_ARGS);
 Datum pcpatch_as_text(PG_FUNCTION_ARGS);
 Datum pcpoint_as_bytea(PG_FUNCTION_ARGS);
-Datum pcpatch_bytea_envelope(PG_FUNCTION_ARGS);
+Datum pcpatch_envelope_as_bytea(PG_FUNCTION_ARGS);
 
 
 static void
@@ -293,8 +293,8 @@ Datum pcpoint_as_bytea(PG_FUNCTION_ARGS)
 	PG_RETURN_BYTEA_P(wkb);
 }
 
-PG_FUNCTION_INFO_V1(pcpatch_bytea_envelope);
-Datum pcpatch_bytea_envelope(PG_FUNCTION_ARGS)
+PG_FUNCTION_INFO_V1(pcpatch_envelope_as_bytea);
+Datum pcpatch_envelope_as_bytea(PG_FUNCTION_ARGS)
 {
 	uint8 *bytes;
 	size_t bytes_size;
@@ -303,13 +303,13 @@ Datum pcpatch_bytea_envelope(PG_FUNCTION_ARGS)
 	SERIALIZED_PATCH *serpatch = PG_GETHEADER_SERPATCH_P(0);
 	PCSCHEMA *schema = pc_schema_from_pcid(serpatch->pcid, fcinfo);
 
-	bytes = pc_patch_to_geometry_wkb_envelope(serpatch, schema, &bytes_size);
+	bytes = pc_bounds_to_wkb(&serpatch->bounds, schema->srid, &bytes_size);
 	wkb_size = VARHDRSZ + bytes_size;
 	wkb = palloc(wkb_size);
 	memcpy(VARDATA(wkb), bytes, bytes_size);
 	SET_VARSIZE(wkb, wkb_size);
 
-	pfree(bytes);
+	pcfree(bytes);
 
 	PG_RETURN_BYTEA_P(wkb);
 }
