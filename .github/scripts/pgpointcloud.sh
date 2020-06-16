7600c71228b1b8fd9f150308237f4db34f8d8705
Blottiere Paul <blottiere.paul@gmail.com>
2020-06-16 22:20:37 +0200

    Update github actions

--- .github/scripts/pgpointcloud.sh
+++ .github/scripts/pgpointcloud.sh
@@ -2,11 +2,9 @@
 
 set -e
 
-cd ../.. && \
-    ./tools/build-install.sh && \
-    sh .install-lazperf.sh && \
-    ./tools/build-install.sh --with-lazperf=/usr/local && \
-    make check && \
-    ./tools/valgrind.sh && \
-    make installcheck && \
-    cat pgsql/regression.diffs || true
+./tools/build-install.sh
+sh .install-lazperf.sh
+./tools/build-install.sh --with-lazperf=/usr/local
+make check
+./tools/valgrind.sh
+make installcheck
