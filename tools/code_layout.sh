f474049fad3b00255abb5712398c4dc376a3a176
Blottiere Paul <blottiere.paul@gmail.com>
2022-02-07 17:12:59 +0100

    Format hpp and cpp files too

--- tools/code_layout.sh
+++ tools/code_layout.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-clang-format -i --style=llvm lib/*.c lib/*.h lib/cunit/*.c lib/cunit/*.h
+clang-format -i --style=llvm lib/*.c lib/*.h lib/cunit/*.c lib/*.cpp lib/*.hpp lib/cunit/*.h
