0423427f9b54fb71d54357edbac5bfa38d8df11d
Blottiere Paul <blottiere.paul@gmail.com>
2022-02-07 17:13:29 +0100

    clang-format 4

--- lib/lazperf_adapter.h
+++ lib/lazperf_adapter.h
@@ -1,14 +1,14 @@
 /***********************************************************************
-* lazperf_adapter.h
-*
-*  PgSQL Pointcloud is free and open source software provided
-*  by the Government of Canada
-*
-*  Copyright (c) 2013 Natural Resources Canada
-*  Copyright (c) 2013 OpenGeo
-*  Copyright (c) 2017 Oslandia
-*
-***********************************************************************/
+ * lazperf_adapter.h
+ *
+ *  PgSQL Pointcloud is free and open source software provided
+ *  by the Government of Canada
+ *
+ *  Copyright (c) 2013 Natural Resources Canada
+ *  Copyright (c) 2013 OpenGeo
+ *  Copyright (c) 2017 Oslandia
+ *
+ ***********************************************************************/
 
 #ifndef _LAZPERF_ADAPTER_H
 #define _LAZPERF_ADAPTER_H
@@ -16,8 +16,10 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-size_t lazperf_compress_from_uncompressed(const PCPATCH_UNCOMPRESSED *pa, uint8_t **compressed);
-size_t lazperf_uncompress_from_compressed(const PCPATCH_LAZPERF *pa, uint8_t **decompressed);
+size_t lazperf_compress_from_uncompressed(const PCPATCH_UNCOMPRESSED *pa,
+                                          uint8_t **compressed);
+size_t lazperf_uncompress_from_compressed(const PCPATCH_LAZPERF *pa,
+                                          uint8_t **decompressed);
 #ifdef __cplusplus
 }
 #endif
